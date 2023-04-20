26eb758964b51a29c917ed80abe161ee2742c8ff
Bastian Hodapp <bastianhodapp@gmail.com>
2023-04-20 21:36:09 +0200

    install vuetify

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,27 @@ import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
 
-createApp(App).mount('#app');
+// Vuetify
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import { aliases, mdi } from 'vuetify/iconsets/mdi';
+
+const vue = createApp(App);
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+            mdi,
+        },
+    },
+});
+
+vue.use(vuetify);
+
+vue.mount('#app');
