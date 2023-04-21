8645dc43364185adf61663751355a5734a9241c5
Bastian Hodapp <bastianhodapp@gmail.com>
2023-04-21 14:23:44 +0200

    add working map

---
+++ src/components/Map.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="leaflet-map">
+        <l-map :zoom="15" :center="[48.050857, 8.207022]">
+            <l-tile-layer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                layer-type="base"
+                name="OpenStreetMap"
+            ></l-tile-layer>
+        </l-map>
+    </div>
+</template>
+
+<script setup lang="ts">
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import { LMap, LTileLayer } from '@vue-leaflet/vue-leaflet';
+</script>
+
+<style>
+.leaflet-map {
+    width: 100%;
+    height: 100%;
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), vuetify(), tsconfigPaths()],
+    plugins: [tsconfigPaths(), vue(), vuetify()],
 });
