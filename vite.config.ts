4b906e8ec9508619a584c8224dde9a706bed3420
Bastian Hodapp <bastianhodapp@gmail.com>
2023-04-21 13:35:52 +0200

    install axios and vite paths plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vuetify from 'vite-plugin-vuetify';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), vuetify()],
+    plugins: [vue(), vuetify(), tsconfigPaths()],
 });
