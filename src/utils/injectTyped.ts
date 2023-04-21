6aae05769f30c1e3c56825383e8c82bf3ab7a858
Bastian Hodapp <bastianhodapp@gmail.com>
2023-04-21 20:22:59 +0200

    add comment with code source

--- src/utils/injectTyped.ts
+++ src/utils/injectTyped.ts
@@ -1,3 +1,4 @@
+// https://dev.to/avxkim/using-axios-globally-in-a-vue-3-with-provideinject-composition-api-1jk5
 import { inject, InjectionKey } from 'vue';
 
 export function injectStrict<T>(key: InjectionKey<T>, fallback?: T) {
