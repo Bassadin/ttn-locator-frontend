575691b220a7bb0fedc3c7e8d8ebbe77ab68ba34
Bastian Hodapp <bastianhodapp@gmail.com>
2023-04-22 14:08:02 +0200

    add sidebar navigation

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
 <template>
-    <router-view></router-view>
+    <v-layout full-height>
+        <v-navigation-drawer rail expand-on-hover>
+            <v-list>
+                <v-list-item prepend-icon="mdi-map-marker" title="TTN Locator"></v-list-item>
+            </v-list>
+
+            <v-divider></v-divider>
+
+            <v-list density="compact" nav>
+                <v-list-item prepend-icon="mdi-home" title="Overview" value="myfiles"></v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+
+        <v-main class="h-full">
+            <router-view></router-view>
+        </v-main>
+    </v-layout>
 </template>
 
 <style scoped>
