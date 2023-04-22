4597e54d8b458a64d52b3a35fdcee722738cc506
Bastian Hodapp <bastianhodapp@gmail.com>
2023-04-22 13:54:44 +0200

    import icon package

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -3,6 +3,10 @@ import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
+
+// Icons
+// https://vuetifyjs.com/en/features/icon-fonts/#material-design-icons
+import '@mdi/font/css/materialdesignicons.css';
 import { aliases, mdi } from 'vuetify/iconsets/mdi';
 
 export default createVuetify({
