450e5a8010a8b31d2a713002c482555a4566e14b
Bastian Hodapp <bastianhodapp@gmail.com>
2023-04-23 12:52:32 +0200

    disable the deafult zoom controls

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="leaflet-map">
-        <l-map :zoom="15" :center="[48.050857, 8.207022]">
+        <l-map :zoom="15" :center="[48.050857, 8.207022]" :options="{ zoomControl: false }">
             <l-tile-layer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 layer-type="base"
@@ -10,13 +10,14 @@
             <DeviceGPSDatapointsLayer />
 
             <l-control-attribution position="topright" prefix="Data courtesy of TTN Mapper"></l-control-attribution>
+            <l-control-zoom position="topright"></l-control-zoom>
         </l-map>
     </div>
 </template>
 
 <script setup lang="ts">
 import 'leaflet/dist/leaflet.css';
-import { LMap, LTileLayer, LControlAttribution } from '@vue-leaflet/vue-leaflet';
+import { LMap, LTileLayer, LControlAttribution, LControlZoom } from '@vue-leaflet/vue-leaflet';
 
 // Components
 import GatewayLayer from '@/components/map/GatewayLayer.vue';
