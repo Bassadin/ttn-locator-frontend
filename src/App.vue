55150ac6c0fe2c61c326197e1d48c6d947af6c91
Bastian Hodapp <bastianhodapp@gmail.com>
2023-05-02 17:45:29 +0200

    move navigation into own component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,7 @@
 <template>
     <v-app>
         <v-layout full-height>
-            <v-app-bar :elevation="2" color="primary">
-                <v-app-bar-nav-icon variant="text" @click="showNavDrawer = !showNavDrawer" />
-                <v-app-bar-title>TTN Locator</v-app-bar-title>
-                <v-spacer></v-spacer>
-                <color-switcher-button />
-            </v-app-bar>
-            <v-navigation-drawer v-model="showNavDrawer" :rail="!mobile" :expand-on-hover="!mobile">
-                <v-list density="compact" nav>
-                    <v-list-item prepend-icon="mdi-home" title="Home" to="/"></v-list-item>
-                    <v-list-item
-                        prepend-icon="mdi-router-wireless"
-                        title="Gateway Range (RSSI)"
-                        to="/gateway_rssi"
-                    ></v-list-item>
-                    <v-list-item
-                        prepend-icon="mdi-chart-scatter-plot"
-                        title="RSSI Range Graph"
-                        to="/rssi_range_graph"
-                    ></v-list-item>
-                </v-list>
-            </v-navigation-drawer>
-
+            <main-navigation-drawer />
             <v-main class="h-full">
                 <router-view></router-view>
             </v-main>
@@ -31,20 +10,7 @@
 </template>
 
 <script lang="ts" setup>
-import { useTitle } from '@vueuse/core';
-import { ref } from 'vue';
-
-// Components
-import ColorSwitcherButton from './components/ColorSwitcherButton.vue';
-
-// Vuetify
-import { useDisplay } from 'vuetify';
-const { mobile } = useDisplay();
-
-const title = useTitle();
-title.value = 'TTN Locator';
-
-const showNavDrawer = ref(true);
+import MainNavigationDrawer from '@/components/nav/MainNavigation.vue';
 </script>
 
 <style scoped>
---
+++ src/components/nav/MainNavigation.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-app-bar :elevation="2" color="primary">
+        <v-app-bar-nav-icon variant="text" @click="showNavDrawer = !showNavDrawer" />
+        <v-app-bar-title>{{ title }}</v-app-bar-title>
+        <v-spacer></v-spacer>
+        <color-switcher-button />
+    </v-app-bar>
+    <v-navigation-drawer v-model="showNavDrawer" :rail="!mobile" :expand-on-hover="!mobile">
+        <v-list density="compact" nav>
+            <v-list-item prepend-icon="mdi-home" title="Home" to="/"></v-list-item>
+            <v-list-item
+                prepend-icon="mdi-router-wireless"
+                title="Gateway Range (RSSI)"
+                to="/gateway_rssi"
+            ></v-list-item>
+            <v-list-item
+                prepend-icon="mdi-chart-scatter-plot"
+                title="RSSI Range Graph"
+                to="/rssi_range_graph"
+            ></v-list-item>
+        </v-list>
+    </v-navigation-drawer>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { useTitle } from '@vueuse/core';
+
+// Components
+import ColorSwitcherButton from '@/components/ColorSwitcherButton.vue';
+
+// Vuetify
+import { useDisplay } from 'vuetify';
+const { mobile } = useDisplay();
+
+const showNavDrawer = ref(true);
+
+const title = useTitle();
+title.value = 'TTN Locator';
+</script>
