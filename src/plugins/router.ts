0268b025b0bdfce4209a10f19a5567e98375ba77
Bastian Hodapp <bastianhodapp@gmail.com>
2023-05-04 22:38:35 +0200

    add 404 page

--- src/plugins/router.ts
+++ src/plugins/router.ts
@@ -5,6 +5,8 @@ const routes = [
     { path: '/gateway_rssi', component: () => import('@/views/GatewayRSSI.vue') },
     { path: '/rssi_range_graph', component: () => import('@/views/GatewayRSSIRangeGraph.vue') },
     { path: '/manage/devices', component: () => import('@/views/manage/DevicesManage.vue') },
+    // Catch all 404
+    { path: '/:pathMatch(.*)*', name: 'not_found', component: () => import('@/views/NotFound.vue') },
 ];
 
 export default createRouter({
---
+++ src/views/NotFound.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { Ref, ref } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+const notFoundPageName: Ref<string> = ref(route.params.pathMatch.toString());
+</script>
+
+<template>
+    <!-- 404 page -->
+    <v-container>
+        <v-row>
+            <v-col>
+                <v-card>
+                    <v-card-title>404</v-card-title>
+                    <v-card-text>
+                        <p>Page '{{ notFoundPageName }}'' not found</p>
+                    </v-card-text>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
