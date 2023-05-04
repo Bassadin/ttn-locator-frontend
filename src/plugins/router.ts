909bbd8e6b4604fb6514a1be1df7e42067c7a566
Bastian Hodapp <bastianhodapp@gmail.com>
2023-05-04 22:38:45 +0200

    name all routes

--- src/plugins/router.ts
+++ src/plugins/router.ts
@@ -1,10 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
-    { path: '/', component: () => import('@/views/HomePage.vue') },
-    { path: '/gateway_rssi', component: () => import('@/views/GatewayRSSI.vue') },
-    { path: '/rssi_range_graph', component: () => import('@/views/GatewayRSSIRangeGraph.vue') },
-    { path: '/manage/devices', component: () => import('@/views/manage/DevicesManage.vue') },
+    { path: '/', name: 'home', component: () => import('@/views/HomePage.vue') },
+    { path: '/gateway_rssi', name: 'gateway_rssi', component: () => import('@/views/GatewayRSSI.vue') },
+    {
+        path: '/rssi_range_graph/:gatewayID',
+        name: 'rssi_range_graph',
+        component: () => import('@/views/GatewayRSSIRangeGraph.vue'),
+    },
+    { path: '/manage/devices', name: 'manage_devices', component: () => import('@/views/manage/DevicesManage.vue') },
     // Catch all 404
     { path: '/:pathMatch(.*)*', name: 'not_found', component: () => import('@/views/NotFound.vue') },
 ];
