a0bead00cb90c03c09925c4d80b740ddbe3d225b
Bastian Hodapp <bastianhodapp@gmail.com>
2023-05-08 17:03:33 +0200

    remove unnecessary component code

--- src/components/LoadingOverlay.vue
+++ src/components/LoadingOverlay.vue
@@ -1,21 +1,5 @@
 <template>
-    <v-overlay v-model="isCurrentlyLoading" class="align-center justify-center" persistent>
+    <v-overlay class="align-center justify-center" persistent>
         <v-progress-circular indeterminate size="64"></v-progress-circular
     ></v-overlay>
 </template>
-
-<script setup lang="ts">
-import { computed } from 'vue';
-
-const props = defineProps({ modelValue: { type: Boolean, required: true } });
-const emits = defineEmits(['update:modelValue']);
-
-const isCurrentlyLoading = computed({
-    get() {
-        return props.modelValue;
-    },
-    set(value) {
-        emits('update:modelValue', value);
-    },
-});
-</script>
