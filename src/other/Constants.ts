9492250e797f3d521fb931013833f0ef010972c6
Bastian Hodapp <bastianhodapp@gmail.com>
2023-05-09 21:15:00 +0200

    force number type

--- src/other/Constants.ts
+++ src/other/Constants.ts
@@ -1,4 +1,4 @@
 export default class Constants {
-    public static readonly MIN_SELECTABLE_RSSI = -120;
-    public static readonly MAX_SELECTABLE_RSSI = 0;
+    public static readonly MIN_SELECTABLE_RSSI: number = -120;
+    public static readonly MAX_SELECTABLE_RSSI: number = 0;
 }
