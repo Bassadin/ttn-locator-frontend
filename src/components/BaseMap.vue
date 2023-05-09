57c4653e4408a1555a8f754b25c98c52d6ad07b0
Bastian Hodapp <bastianhodapp@gmail.com>
2023-05-09 15:15:04 +0200

    add more layers

--- src/components/BaseMap.vue
+++ src/components/BaseMap.vue
@@ -1,7 +1,16 @@
 <template>
     <div class="leaflet-map">
-        <l-map :zoom="15" :center="[48.050857, 8.207022]" :options="{ zoomControl: false }">
-            <l-tile-layer :url="mapTileUrl" layer-type="base" name="OpenStreetMap" />
+        <l-map :zoom="15" :center="mapCenter" :options="{ zoomControl: false }">
+            <l-tile-layer
+                v-for="tileProvider in tileProviders"
+                :key="tileProvider.name"
+                :name="tileProvider.name"
+                :visible="tileProvider.visible"
+                :url="tileProvider.url"
+                :attribution="tileProvider.attribution"
+                layer-type="base"
+            />
+
             <slot name="map-layers"></slot>
 
             <l-control-attribution
@@ -11,16 +20,34 @@
 
             <slot name="map-controls">
                 <l-control-zoom position="topright"></l-control-zoom>
+                <l-control-layers position="topright"></l-control-layers>
             </slot>
         </l-map>
     </div>
 </template>
 
 <script setup lang="ts">
-import 'leaflet/dist/leaflet.css';
-import { LMap, LTileLayer, LControlAttribution, LControlZoom } from '@vue-leaflet/vue-leaflet';
+import { ref } from 'vue';
 
-const mapTileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+import 'leaflet/dist/leaflet.css';
+import { LMap, LTileLayer, LControlAttribution, LControlZoom, LControlLayers } from '@vue-leaflet/vue-leaflet';
+
+const mapCenter = ref([48.050857, 8.207022]);
+const tileProviders = ref([
+    {
+        name: 'OpenStreetMap',
+        visible: true,
+        attribution: '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+        url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    },
+    {
+        name: 'OpenTopoMap',
+        visible: false,
+        url: 'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
+        attribution:
+            'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
+    },
+]);
 </script>
 
 <style>
