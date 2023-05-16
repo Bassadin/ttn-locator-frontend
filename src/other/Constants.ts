7382386b7f8c42c5d0deec548b5f2167eb0b8d1e
Bastian Hodapp <bastianhodapp@gmail.com>
2023-05-16 20:06:29 +0200

    adjust some constants

--- src/other/Constants.ts
+++ src/other/Constants.ts
@@ -1,6 +1,6 @@
 export default class Constants {
-    public static readonly MIN_SELECTABLE_RSSI: number = -120;
+    public static readonly MIN_SELECTABLE_RSSI: number = -150;
     public static readonly MAX_SELECTABLE_RSSI: number = 0;
     public static readonly HDOP_CUTOFF_POINT: number = 1.5;
-    public static readonly DEFAULT_RSSI_CHECKING_RANGE: number = 5;
+    public static readonly DEFAULT_RSSI_CHECKING_RANGE: number = 3;
 }
