df65915d15db0b81995cea9b46795bd180659ad5
Bastian Hodapp <bastianhodapp@gmail.com>
2023-05-22 13:59:19 +0200

    fix opentopomap url

--- src/components/BaseMap.vue
+++ src/components/BaseMap.vue
@@ -58,7 +58,7 @@ const tileProviders = ref([
     {
         name: 'OpenTopoMap',
         visible: false,
-        url: 'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
+        url: 'https://a.tile.opentopomap.org/{z}/{x}/{y}.png',
         attribution:
             'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
     },
