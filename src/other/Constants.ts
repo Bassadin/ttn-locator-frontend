65ef4eba8ab585a8a71cba7ca37d7ab2792f9efe
Bastian Hodapp <bastianhodapp@gmail.com>
2023-08-01 17:31:17 +0200

    add constants for spreading factor selection

--- src/other/Constants.ts
+++ src/other/Constants.ts
@@ -1,9 +1,16 @@
 export default class Constants {
     public static readonly MIN_SELECTABLE_RSSI: number = -150;
     public static readonly MAX_SELECTABLE_RSSI: number = 0;
+
     public static readonly MIN_SELECTABLE_SNR: number = -20;
     public static readonly MAX_SELECTABLE_SNR: number = 20;
+
+    public static readonly DEFAULT_SF_VALUE: number = 7;
+    public static readonly MIN_SELECTABLE_SF: number = 7;
+    public static readonly MAX_SELECTABLE_SF: number = 12;
+
     public static readonly HDOP_CUTOFF_POINT: number = 1.5;
+
     public static readonly DEFAULT_RSSI_CHECKING_RANGE: number = 3;
     public static readonly DEFAULT_SNR_CHECKING_RANGE: number = 2;
     public static readonly DEFAULT_RSSI_TO_RANGE_TOLERANCE_METERS: number = 100;
